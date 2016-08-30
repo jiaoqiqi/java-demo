bfde291625882d501ac68b3eee1bc93f3a18a24e
jiaoqiqi <1176932165@qq.com>
2016-08-31 00:03:18 +0800

    add puppy age setting

--- src/Puppy.java
+++ src/Puppy.java
@@ -1,11 +1,27 @@
 public class Puppy {
     public Puppy(String name){
 
-        System.out.print("Puppy name is " + name);
+        System.out.println("Puppy name is " + name);
+    }
+
+    int puppyAge;
+
+    public void setAge(int age){
+        puppyAge = age ;
+    }
+
+    public int getAge(){
+        System.out.println("Puppy's age is " + puppyAge);
+        return puppyAge;
     }
 
     public static  void main(String []args){
 
         Puppy myPuppy = new Puppy("tommy");
+
+        myPuppy.setAge(2);
+        myPuppy.getAge();
+
+        System.out.println("variable value :" + myPuppy.puppyAge);
     }
 }
