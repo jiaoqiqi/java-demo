296cf32d93629d93cd1f062e31b482de473f7e06
jiaoqiqi <1176932165@qq.com>
2016-08-31 21:11:40 +0800

    try class and object

---
+++ src/Practice.java
@@ -0,0 +1,13 @@
+public class Practice {
+   public Practice(int m, int r){
+      float result = m*r*r;
+       System.out.println(result);
+   }
+
+    public static void main(String []args){
+        Practice myPractice = new Practice(2,3);
+
+    }
+
+}
+
