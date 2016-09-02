6e8795c9eee4102b6f8a1a14fdfab649ffa49f18
jiaoqiqi <1176932165@qq.com>
2016-09-03 01:28:24 +0800

    add judge two circle distance

--- src/TwoCircle.java
+++ src/TwoCircle.java
@@ -1,7 +1,28 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-/**
- * Created by jiaoqiqi on 16-9-2.
- */
 public class TwoCircle {
+    public static void main(String []args){
+        System.out.println("Enter circle1`'s center x-,y-coordinates, and radius: ");
+        Scanner input = new Scanner(System.in);
+
+        double x1 = input.nextDouble();
+        double y1 = input.nextDouble();
+        double r1 = input.nextDouble();
+
+        System.out.println("Enter circle2's center x-,y-coordinates, and radius: ");
+        double x2 = input.nextDouble();
+        double y2 = input.nextDouble();
+        double r2 = input.nextDouble();
+
+        double distance = Math.sqrt((y2-y1)*(y2-y1) + (x2-x1)*(x2-x1));
+
+        if(distance <= (Math.abs(r1-r2)) ){
+            System.out.println("circle is inside circle1");
+        }else if(distance <= r1+r2 ){
+            System.out.println("circle2 overlaps circle1");
+        }
+        else{
+            System.out.println("circle2 does not overlap circle");
+        }
+    }
 }
