e089611c3344666c17c8d561cf725faca171cb68
jiaoqiqi <1176932165@qq.com>
2016-09-02 13:35:03 +0800

    add payRoll with GUI

--- src/PayrollWithGUI.java
+++ src/PayrollWithGUI.java
@@ -1,27 +1,27 @@
 import javax.swing.*;
 
-public class Payroll {
+public class PayrollWithGUI {
     public static  void main(String []args){
 
         String employeeName = JOptionPane.showInputDialog(null,"Enter employee's name: ");
-        JOptionPane.showMessageDialog(null,"Enter employee's name: "+employeeName);
+//        JOptionPane.showMessageDialog(null,"Enter employee's name: "+employeeName);
 
         String workHour = JOptionPane.showInputDialog(null,"Enter number of hours worked in a week: ");
-        JOptionPane.showMessageDialog(null,"Enter number of hours worked in a week: "+workHour);
+//        JOptionPane.showMessageDialog(null,"Enter number of hours worked in a week: "+workHour);
 
         String hourlyPayRate = JOptionPane.showInputDialog(null,"Enter hourly pay rate: ");
-        JOptionPane.showMessageDialog(null,"Enter hourly pay rate: "+hourlyPayRate);
+//        JOptionPane.showMessageDialog(null,"Enter hourly pay rate: "+hourlyPayRate);
 
         String federalTax = JOptionPane.showInputDialog(null,"Enter faderal tax withholding rate: ");
-        JOptionPane.showMessageDialog(null,"Enter faderal tax withholding rate: "+federalTax);
+//        JOptionPane.showMessageDialog(null,"Enter faderal tax withholding rate: "+federalTax);
 
         String stateTax = JOptionPane.showInputDialog(null,"Enter state tax withholding rate: ");
-        JOptionPane.showMessageDialog(null,"Enter state tax withholding rate: "+stateTax);
+//        JOptionPane.showMessageDialog(null,"Enter state tax withholding rate: "+stateTax);
 
         double grossPay = Double.parseDouble(hourlyPayRate)  * Double.parseDouble( workHour);
         double federalWithholding = grossPay * Double.parseDouble(federalTax);
-        double StateWithholdig = grossPay * Double.parseDouble(stateTax);
-        double totalDeduction = federalWithholding+StateWithholdig;
+        double StateWithholding = grossPay * Double.parseDouble(stateTax);
+        double totalDeduction = federalWithholding+StateWithholding;
         double netPay = grossPay-totalDeduction;
 
         JOptionPane.showMessageDialog(null,"Employee Name: " + employeeName);
@@ -30,9 +30,10 @@ public class Payroll {
         JOptionPane.showMessageDialog(null,"Gross rate:  $" + grossPay);
         JOptionPane.showMessageDialog(null,"Deductions:");
         JOptionPane.showMessageDialog(null,"Federal Withholding(" + Double.parseDouble(federalTax)*100 + "%):  $" +  federalWithholding);
-        JOptionPane.showMessageDialog(null,"State Withholding(" + Double.parseDouble(federalTax)*100 + "%):  $" +  StateWithholdig);
+        JOptionPane.showMessageDialog(null,"State Withholding(" + Double.parseDouble(stateTax)*100 + "%):  $" +  StateWithholding);
         JOptionPane.showMessageDialog(null,"Total Deduction:  $" + totalDeduction);
         JOptionPane.showMessageDialog(null,"Net Pay:  $" + netPay);
+
 
     }
 }
