4aa6f3f8a61915be5cba35475456f27f6bebed1d
jiaoqiqi <1176932165@qq.com>
2016-09-03 01:26:56 +0800

    use System.out.println once

--- src/PayrollWithConsole.java
+++ src/PayrollWithConsole.java
@@ -4,24 +4,20 @@ public class PayrollWithConsole {
     public static  void main(String []args){
 
         System.out.print("Enter employee's name: ");
-        Scanner input1 = new Scanner(System.in);
-        String employeeName = input1.next();
+        Scanner input = new Scanner(System.in);
+        String employeeName = input.next();
 
         System.out.print("Enter number of hours worked in a week: ");
-        Scanner input2  = new Scanner(System.in);
-        int workHour = input2.nextInt();
+        int workHour = input.nextInt();
 
         System.out.print("Enter hourly pay rate: ");
-        Scanner input3  = new Scanner(System.in);
-        double hourlyPayRate = input3.nextDouble();
+        double hourlyPayRate = input.nextDouble();
 
-        System.out.print("Enter faderal tax withholding rate: ");
-        Scanner input4  = new Scanner(System.in);
-        double federalTax = input4.nextDouble();
+        System.out.print("Enter federal tax withholding rate: ");
+        double federalTax = input.nextDouble();
 
         System.out.print("Enter state tax withholding rate: ");
-        Scanner input5  = new Scanner(System.in);
-        double stateTax = input5.nextDouble();
+        double stateTax = input.nextDouble();
 
         double grossPay = hourlyPayRate  *  workHour;
         double federalWithholding = grossPay * federalTax;
@@ -30,14 +26,15 @@ public class PayrollWithConsole {
         double netPay = grossPay-totalDeduction;
 
 
-        System.out.println("Employee Name: " + employeeName);
-        System.out.println("Hours Worked: " + workHour);
-        System.out.println("Pay rate:  $" + hourlyPayRate);
-        System.out.println("Gross rate:  $" + grossPay);
-        System.out.println("Deductions:");
-        System.out.println("  Federal Withholding(" + federalTax*100 + "%):  $" +  federalWithholding);
-        System.out.println("  State Withholding(" + stateTax*100 + "%):  $" +  StateWithholding);
-        System.out.println("  Total Deduction:  $" + totalDeduction);
-        System.out.println("Net Pay:  $" + netPay);
+        System.out.println("Employee Name: " + employeeName + "\n" +
+                "Hours Worked: " + workHour + "\n" +
+                "Pay rate:  $" + hourlyPayRate + "\n" +
+                "Gross rate:  $" + grossPay + "\n" +
+                "Deductions:" + "\n" +
+                "  Federal Withholding(" + federalTax*100 + "%):  $" +  federalWithholding + "\n" +
+                "  State Withholding(" + stateTax*100 + "%):  $" +  StateWithholding + "\n" +
+                "  Total Deduction:  $" + totalDeduction + "\n" +
+                "Net Pay:  $" + netPay);
+
     }
 }
