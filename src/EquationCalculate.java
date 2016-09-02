edfdf8165be811a014fcaabfc9ce8011ba62c662
jiaoqiqi <1176932165@qq.com>
2016-09-03 01:23:30 +0800

    use once JOptionPane.showMessageDialog

--- src/EquationCalculate.java
+++ src/EquationCalculate.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class EquationCalculate {
-    public static void main(String []args){
+    public static void main(String[] args) {
         System.out.print("Enter a,b,c: ");
 
         Scanner input = new Scanner(System.in);
@@ -9,18 +9,19 @@ public class EquationCalculate {
         double b = input.nextDouble();
         double c = input.nextDouble();
 
-        double q = Math.sqrt(b*b - 4*a*c);
-         if(q>0){
-             double r1 = (-b+q)/(2*a);
-             double r2 = (-b-q)/(2*a);
-             System.out.println("The roots are " + String.format("%.6f",r1) + " and " + String.format("%.5f" , r2));
-         }
-         else if(q==0){
-             double r = (-b+q)/(2*a);
-             System.out.println("The roots is " + r);
-         }
-         else if( q < 0 ){
-             System.out.println("The equation has no real roots");
-         }
+        double q = Math.sqrt(b * b - 4 * a * c);
+        if (q > 0) {
+            double r1 = (-b + q) / (2 * a);
+            double r2 = (-b - q) / (2 * a);
+            System.out.println("The roots are " + String.format("%.6f", r1) + " and "
+                    + String.format("%.5f", r2));
+        } else if (q == 0) {
+            double r = (-b + q) / (2 * a);
+            System.out.println("The roots is " + r);
+        } else if (q < 0) {
+            System.out.println("The equation has no real roots");
+        }
     }
 }
+
+
--- src/PayrollWithGUI.java
+++ src/PayrollWithGUI.java
@@ -4,19 +4,10 @@ public class PayrollWithGUI {
     public static  void main(String []args){
 
         String employeeName = JOptionPane.showInputDialog(null,"Enter employee's name: ");
-//        JOptionPane.showMessageDialog(null,"Enter employee's name: "+employeeName);
-
         String workHour = JOptionPane.showInputDialog(null,"Enter number of hours worked in a week: ");
-//        JOptionPane.showMessageDialog(null,"Enter number of hours worked in a week: "+workHour);
-
         String hourlyPayRate = JOptionPane.showInputDialog(null,"Enter hourly pay rate: ");
-//        JOptionPane.showMessageDialog(null,"Enter hourly pay rate: "+hourlyPayRate);
-
-        String federalTax = JOptionPane.showInputDialog(null,"Enter faderal tax withholding rate: ");
-//        JOptionPane.showMessageDialog(null,"Enter faderal tax withholding rate: "+federalTax);
-
+        String federalTax = JOptionPane.showInputDialog(null,"Enter federal tax withholding rate: ");
         String stateTax = JOptionPane.showInputDialog(null,"Enter state tax withholding rate: ");
-//        JOptionPane.showMessageDialog(null,"Enter state tax withholding rate: "+stateTax);
 
         double grossPay = Double.parseDouble(hourlyPayRate)  * Double.parseDouble( workHour);
         double federalWithholding = grossPay * Double.parseDouble(federalTax);
@@ -24,16 +15,15 @@ public class PayrollWithGUI {
         double totalDeduction = federalWithholding+StateWithholding;
         double netPay = grossPay-totalDeduction;
 
-        JOptionPane.showMessageDialog(null,"Employee Name: " + employeeName);
-        JOptionPane.showMessageDialog(null,"Hours Worked: " + Double.parseDouble(workHour));
-        JOptionPane.showMessageDialog(null,"Pay rate:  $" + Double.parseDouble(hourlyPayRate));
-        JOptionPane.showMessageDialog(null,"Gross rate:  $" + grossPay);
-        JOptionPane.showMessageDialog(null,"Deductions:");
-        JOptionPane.showMessageDialog(null,"Federal Withholding(" + Double.parseDouble(federalTax)*100 + "%):  $" +  federalWithholding);
-        JOptionPane.showMessageDialog(null,"State Withholding(" + Double.parseDouble(stateTax)*100 + "%):  $" +  StateWithholding);
-        JOptionPane.showMessageDialog(null,"Total Deduction:  $" + totalDeduction);
-        JOptionPane.showMessageDialog(null,"Net Pay:  $" + netPay);
-
+        JOptionPane.showMessageDialog(null,"Employee Name: " + employeeName+"\n" +
+                "Hours Worked: " + Double.parseDouble(workHour)+"\n" +
+                "Pay rate:  $" + Double.parseDouble(hourlyPayRate)+"\n" +
+                "Gross rate:  $" + grossPay + "\n" +
+                "Deductions:" + "\n" +
+                "Federal Withholding(" + Double.parseDouble(federalTax)*100 + "%):  $" +  federalWithholding + "\n" +
+                "State Withholding(" + Double.parseDouble(stateTax)*100 + "%):  $" +  StateWithholding + "\n" +
+                "Total Deduction:  $" + totalDeduction + "\n" +
+                "Net Pay:  $" + netPay);
 
     }
 }
