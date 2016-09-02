a4b3e78fc1575f2b3d73340fddd0a6b1266e1b64
jiaoqiqi <1176932165@qq.com>
2016-09-02 13:51:33 +0800

    add Triangle area

--- src/TriangleArea.java
+++ src/TriangleArea.java
@@ -1,7 +1,55 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-/**
- * Created by jiaoqiqi on 16-9-2.
- */
-public class TriangleArea {
+class  Trangle {
+
+    Scanner input = new Scanner(System.in);
+    float x1 = input.nextFloat();
+    float y1 = input.nextFloat();
+    float x2 = input.nextFloat();
+    float y2 = input.nextFloat();
+    float x3 = input.nextFloat();
+    float y3 = input.nextFloat();
+
+    double x = Math.sqrt((y2-y1)*(y2-y1) + (x2-x1)*(x2-x1));
+    double y = Math.sqrt((y3-y1)*(y3-y1) + (x3-x1)*(x3-x1));
+    double z = Math.sqrt((y3-y2)*(y3-y2) + (x3-x2)*(x3-x2));
+
+
+    public boolean check()
+    {
+        if (x+y>z&&x-y<z)
+            return true;
+        else
+            return false;
+    }
+
+    public void Area()//计算面积并负责输出面积
+    {
+        double p;
+        double s;
+        p=(x+y+z)/2;
+        s=Math.sqrt(p*(p-x)*(p-y)*(p-z));
+
+        System.out.println("The area of the trangle is " +String.format("%.1f", s));
+    }
+
 }
+public class TriangleArea
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Enter three points for a triangle");
+
+        Trangle trangle=new Trangle();
+        if (trangle.check()==true)
+        {
+
+            trangle.Area();
+        }
+        else
+        {
+            System.out.println("wrong input");
+        }
+
+    }
+}  
