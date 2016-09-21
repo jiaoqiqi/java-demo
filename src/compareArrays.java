9345c48c361b80e5a8fe958b3bc7f7316bbae111
jiaoqiqi <1176932165@qq.com>
2016-09-21 23:36:52 +0800

    add compare two arrays

--- src/compareArrays.java
+++ src/compareArrays.java
@@ -1,7 +1,48 @@
-package PACKAGE_NAME;
+import java.util.*;
 
-/**
- * Created by jiaoqiqi on 16-9-21.
- */
 public class compareArrays {
+    public  boolean equal(int[] list1, int[] list2) {
+        Arrays.sort(list1);
+        Arrays.sort(list2);
+        if (list1.length != list2.length) {
+            return false;
+        }
+        for (int i = 0; i < list1.length; i++) {
+            if (list1[i] != list2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String []args){
+
+        compareArrays compare = new compareArrays();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter list1: ");
+        int listLength = input.nextInt();
+        int [] array = new int[listLength];
+        for(int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+
+        System.out.print("Enter list2: ");
+        int listLength2 = input.nextInt();
+        int [] array2 = new int[listLength2];
+        for(int i = 0; i < array2.length; i++) {
+            array2[i] = input.nextInt();
+
+        }
+
+        if(compare.equal(array,array2)){
+            System.out.println("两数组相同");
+        }
+        else{
+            System.out.println("两数组不同");
+        }
+    }
+
 }
