9516c58c0b4291a9a7e357ce064da28c27b2ab99
jiaoqiqi <1176932165@qq.com>
2016-09-22 19:18:12 +0800

    add class

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,18 +1,18 @@
 public class SelectionSort {
     public static void selectionSort(double[] list){
         for(int i=0 ; i<list.length ; i++){
-            double currrentMax = list[i];
+            double currentMax = list[i];
             int currentMaxIndex = i;
 
             for(int j=i+1 ; j<list.length ; j++){
-                if(currrentMax < list[j]){
-                    currrentMax = list[j];
+                if(currentMax < list[j]){
+                    currentMax = list[j];
                     currentMaxIndex = j;
                 }
             }
             if(currentMaxIndex != i){
                 list[currentMaxIndex] = list[i];
-                list[i] = currrentMax;
+                list[i] = currentMax;
             }
         }
     }
