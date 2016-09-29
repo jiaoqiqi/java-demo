c4d65799b9eab4f8f65a4be335f7606b7f5abbf9
jiaoqiqi <1176932165@qq.com>
2016-09-29 21:10:07 +0800

    add judeg is number

--- src/NumberOrABC.java
+++ src/NumberOrABC.java
@@ -1,7 +1,27 @@
-package PACKAGE_NAME;
-
-/**
- * Created by monster on 16-9-28.
- */
+//import  java.util.regex.Pattern;
 public class NumberOrABC {
+    public static boolean isNumeric(String str){
+        for (int i = str.length();--i>=0;){
+            if (!Character.isLetterOrDigit(str.charAt(i))){
+//                Character.isLetterOrDigit( char ch )
+                return false;
+            }
+        }
+        return true;
+    }
+// 正则判断数字
+//    public static boolean isNumeric(String str){
+//        Pattern pattern = Pattern.compile("[0-9]*");
+//        return pattern.matcher(str).matches();
+//    }
+
+    public static void main(String []args){
+        String a = "abc";
+        System.out.println(a+" "+isNumeric(a));
+
+        String s1 = "Welcome to Java";
+        String s2 = s1.replace("o","abc");
+        System.out.println(s1);
+        System.out.println(s2);
+    }
 }
