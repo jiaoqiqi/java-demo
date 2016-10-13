67c829137d512eb1cb0bbdfb7816de7b42fb8201
jiaoqiqi <1176932165@qq.com>
2016-10-14 07:00:20 +0800

    add big divid five or six

--- src/DivideFive.java
+++ src/DivideFive.java
@@ -1,7 +1,21 @@
-package PACKAGE_NAME;
-
-/**
- * Created by monster on 16-10-14.
- */
+import java.math.BigInteger;
 public class DivideFive {
+    public static void main(String []args){
+        long a = Long.MAX_VALUE;
+        String sc = Long.toString(a);
+        BigInteger big = new BigInteger(sc);
+        BigInteger five = new BigInteger("5");
+        BigInteger six = new BigInteger("6");
+        BigInteger zero = new BigInteger("0");
+        BigInteger one = new BigInteger("1");
+
+        int i =0;
+        while(i<10){
+            if( big.remainder(five).equals(zero) || big.remainder(six).equals(zero)){
+                System.out.println(big);
+                i++;
+            }
+            big = big.add(one);
+        }
+    }
 }
