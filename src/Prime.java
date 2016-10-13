fe47601ed141ffd3c4f21c60f6d2617ae023f146
jiaoqiqi <1176932165@qq.com>
2016-10-14 06:59:39 +0800

    add big prime

--- src/Prime.java
+++ src/Prime.java
@@ -1,7 +1,27 @@
-package PACKAGE_NAME;
+import java.math.BigInteger;
 
-/**
- * Created by monster on 16-10-13.
- */
 public class Prime {
+
+    public static void main(String[] args) {
+//        int count=0;
+//        BigInteger  bi2;
+//        long x = Long.MAX_VALUE;
+//        String sc = Long.toString(x);
+//        BigInteger bi = new BigInteger(sc);
+//        while(count<5) {
+//            bi2 = bi.nextProbablePrime();    //要想严格证明是素数，那就从0循环到bi/2验证下
+//            System.out.println( bi );
+//            count++;
+//        }
+
+        int count = 0;
+        long x = Long.MAX_VALUE;
+        String sc = Long.toString(x);
+        BigInteger big = new BigInteger(sc);
+        while (count < 5) {
+            big = big.nextProbablePrime();    //要想严格证明是素数，那就从0循环到bi/2验证下
+            System.out.println(big);
+            count++;
+        }
+    }
 }
