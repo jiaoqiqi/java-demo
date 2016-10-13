ca39a468925d6bf05aada541f9399c8b301b2f0f
jiaoqiqi <1176932165@qq.com>
2016-10-14 07:01:00 +0800

    add use bigdecimal to calcule e

--- src/Decimal.java
+++ src/Decimal.java
@@ -1,7 +1,21 @@
-package PACKAGE_NAME;
-
-/**
- * Created by monster on 16-10-14.
- */
+import java.math.BigDecimal;
 public class Decimal {
+    public static BigDecimal factorial(long n){
+        BigDecimal result = BigDecimal.ONE;
+        for(int i=1 ; i<=n ; i++){
+            result = result.multiply(new BigDecimal(i + ""));
+        }
+        return result;
+    }
+    public static void main(String []args){
+        BigDecimal one = new BigDecimal("1");
+        long n = 100;
+        BigDecimal e = new BigDecimal("0");
+        for(int i=0 ; i<n ; i++){
+            BigDecimal result = factorial(n);
+            BigDecimal c = one.divide(result,25,BigDecimal.ROUND_CEILING);
+            e=e.add(c);
+        }
+        System.out.println(e);
+    }
 }
