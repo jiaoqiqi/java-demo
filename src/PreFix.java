9b9fb0f4620fe43147117574f32170cfc8f11763
jiaoqiqi <1176932165@qq.com>
2016-10-14 06:59:02 +0800

    add big prime

--- src/PreFix.java
+++ src/PreFix.java
@@ -1,7 +1,33 @@
-package PACKAGE_NAME;
-
-/**
- * Created by monster on 16-10-13.
- */
+import java.util.*;
 public class PreFix {
+
+     public static String  prefix(String s1,String s2){
+        String a1[] = s1.split("");
+        String a2[] = s2.split("");
+        int len1 = a1.length;
+        int len2 = a2.length;
+        int len;
+         String result = "";
+        if(len1>len2){
+            len = len2;
+        }else{ len = len1; }
+         for(int i=0 ; i<len ;i++){
+            if(!a1[i].equals(a2[i])){
+                break;
+            }else{
+                result+= a1[i];
+            }
+         }
+         return (result);
+    }
+
+    public static void main(String args[]){
+        PreFix newPrefix = new PreFix();
+        Scanner input = new Scanner(System.in);
+        String s1 = input.next();
+        String s2 = input.next();
+        String a = newPrefix.prefix(s1,s2);
+        System.out.print(a);
+
+    }
 }
