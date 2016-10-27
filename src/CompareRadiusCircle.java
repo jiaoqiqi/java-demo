649ce2642c00c05f1d7bdbecd91bd92c75ba4438
jiaoqiqi <1176932165@qq.com>
2016-10-27 15:03:54 +0800

    add compare circle use radius

---
+++ src/CompareRadiusCircle.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.util.Date;
+
+public class CompareRadiusCircle {
+    Scanner in = new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("请输入第一个圆形的半径");
+        Circle circle1 = new Circle(in.nextDouble());
+        System.out.println("请输入第二个圆形的半径");
+        Circle circle2 = new Circle(in.nextDouble());
+        System.out.println(circle1.equals(circle2));
+    }
+}
+
+
+class Circle extends GeometricObject implements Comparable{
+    double radius =0;
+    public Circle(){
+    }
+    public Circle(double radius){
+        this.radius=radius;
+    }
+    public boolean equals(Object o){
+        if(radius == ((Circle) o).radius ){
+            return true;
+        }
+        return false;
+    }
+    public int compareTo(Object o) {
+        // TODO Auto-generated method stub
+        if(getArea()>( (Circle) o).getArea()){
+            return 1;
+        }
+        return 0;
+    }
+    public double getArea() {
+        return Math.PI*radius*radius;
+    }
+}
+
+
