596cbb236eae28447fcf244ed74a5dcbdfd193f9
jiaoqiqi <1176932165@qq.com>
2016-11-11 10:15:53 +0800

    add prime with BigInteger  and calculate with BigInteger

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,75 @@
+
+
+//输出从b1开始的b2个素数BigInteger
+//public class Test{
+//    public static void main(String[] args){
+//        Scanner input = new Scanner(System.in);
+//        BigInteger b1 = new BigInteger(input.next());
+//        int b2 = input.nextInt();
+//        for (;b2>0;b1 = b1.add(BigInteger.ONE)){
+//            if (b1.isProbablePrime(100)) {
+//                System.out.println(b1);
+//                --b2;
+//            }
+//        }
+//    }
+//
+//}
+
+
+//输出从1970年算的秒数转化的日期的字符串
+//public class Test{
+//    public static void main(String []args){
+//        Date date = new Date();
+//        System.out.println(date.getTime());
+//        System.out.println(date.toString());
+//    }
+//}
+
 import java.math.BigInteger;
-import java.util.Scanner;
+import java.util.*;
+//四则运算器BigInteger
 public class Test{
-    public static void main(String[] args){
+    public static void main(String []args){
         Scanner input = new Scanner(System.in);
-        BigInteger b1 = new BigInteger(input.next());
-        int b2 = input.nextInt();
-        for (;b2>0;b1 = b1.add(BigInteger.ONE)){
-            if (b1.isProbablePrime(100)) {
-                System.out.println(b1);
-                --b2;
-            }
+        String str = input.next();
+        String[] splited;
+        if((str.indexOf('/') != -1)){
+            splited = str.split("/");
+            String a = splited[0];
+            String b = splited[1];
+            BigInteger first = new BigInteger(a);
+            BigInteger second = new BigInteger(b);
+            BigInteger result = first.divide(second);
+            System.out.println(result);
+        }
+        else if((str.indexOf('+') != -1)){
+            splited = str.split("[+]");
+            String a = splited[0];
+            String b = splited[1];
+            BigInteger first = new BigInteger(a);
+            BigInteger second = new BigInteger(b);
+            BigInteger result = first.add(second);
+            System.out.println(result);
+        }
+        else if((str.indexOf('-') != -1)) {
+            splited = str.split("-");
+            String a = splited[0];
+            String b = splited[1];
+            BigInteger first = new BigInteger(a);
+            BigInteger second = new BigInteger(b);
+            BigInteger result = first.subtract(second);
+            System.out.println(result);
+        }
+        else if((str.indexOf('*') != -1)){
+            splited = str.split("[*]");
+            String a = splited[0];
+            String b = splited[1];
+            BigInteger first = new BigInteger(a);
+            BigInteger second = new BigInteger(b);
+            BigInteger result = first.multiply(second);
+            System.out.println(result);
         }
     }
-
 }
+
