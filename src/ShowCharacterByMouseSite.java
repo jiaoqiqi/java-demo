c1c793f061e35b11060e3a427cdb3b0bf00db018
jiaoqiqi <1176932165@qq.com>
2016-12-02 10:57:57 +0800

    add mouse site

--- src/ShowCharacterByMouseSite.java
+++ src/ShowCharacterByMouseSite.java
@@ -1,7 +1,53 @@
-package PACKAGE_NAME;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-/**
- * Created by monster on 16-12-2.
- */
-public class ShowCharacterByMouseSite {
+public class ShowCharacterByMouseSite extends JFrame {
+    private DisplayPanel panel = new DisplayPanel();
+
+    public ShowCharacterByMouseSite() {
+        add(panel, BorderLayout.CENTER);
+        panel.setFocusable(true);
+    }
+
+    /** Main method */
+    public static void main(String[] args) {
+        JFrame frame = new ShowCharacterByMouseSite();
+        frame.setTitle("Exercise16_11");
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null); // Center the frame
+        frame.setVisible(true);
+    }
+
+    class DisplayPanel extends JPanel {
+        private int x = 20;
+        private int y = 20;
+        private char keyChar = ' '; // Default key
+
+        public DisplayPanel() {
+            // Add key listener
+            this.addKeyListener(new KeyAdapter() {
+                public void keyPressed(KeyEvent e) {
+                    keyChar = e.getKeyChar();
+                    repaint();
+                }
+            });
+
+            // Add mouse motion listener
+            this.addMouseMotionListener(new MouseAdapter() {
+                public void mouseMoved(MouseEvent e) {
+                    x = e.getX();
+                    y = e.getY();
+                }
+            });
+        }
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            g.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+            g.drawString(String.valueOf(keyChar), x, y);
+        }
+    }
 }
